3c2e321867d9aaa452188b6e3fd6390b63e7439f
Sumana <sumanabrite@gmail.com>
2021-07-19 15:13:37 +0200

    third test

---
+++ src/Entity/Categorie.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\CategorieRepository;
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ORM\Entity(repositoryClass=CategorieRepository::class)
+ */
+class Categorie
+{
+    /**
+     * @ORM\Id
+     * @ORM\GeneratedValue
+     * @ORM\Column(type="integer")
+     */
+    private $id;
+
+    /**
+     * @ORM\Column(type="string", length=40)
+     */
+    private $libelle;
+
+    public function getLibelle(): ?string
+    {
+        return $this->libelle;
+    }
+
+    public function setLibelle(string $libelle): self
+    {
+        $this->libelle = $libelle;
+
+        return $this;
+    }
+
+    
+    
+}
